136fabf5cf4a79ab5893728ceb55499064ed31f6
Analia <a.redonda89@gmail.com>
2019-09-10 20:21:08 -0300

    Fin de seccion 4

---
+++ 02-redux-app/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { IncrementarAction, DecrementarAction } from './contador/contador.actions';
+import { AppState } from './app.reducers';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  contador: number;
+
+  constructor( private store: Store<AppState>){
+    this.store.select('contador').subscribe(contador => this.contador = contador);
+  }
+
+  incrementar(){
+    const accion = new IncrementarAction();
+    this.store.dispatch( accion );
+  }
+
+  decrementar(){
+    const accion = new DecrementarAction();
+    this.store.dispatch( accion );
+  }
+}
