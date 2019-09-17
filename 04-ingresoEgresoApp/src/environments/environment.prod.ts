73cd21a28417c1e0a4a36c3f6010adea94e16fe2
Analia <a.redonda89@gmail.com>
2019-09-16 21:04:26 -0300

    Fin seccion 7

--- 04-ingresoEgresoApp/src/environments/environment.prod.ts
+++ 04-ingresoEgresoApp/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBQpNEAF4MZuDzKvgqU-RR6uYLuBtMj4RI',
+    authDomain: 'ingreso-egreso-app-6e1b2.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-6e1b2.firebaseio.com',
+    projectId: 'ingreso-egreso-app-6e1b2',
+    storageBucket: 'ingreso-egreso-app-6e1b2.appspot.com',
+    messagingSenderId: '739814598360'
+  }
 };
--- 04-ingresoEgresoApp/src/environments/environment.ts
+++ 04-ingresoEgresoApp/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBQpNEAF4MZuDzKvgqU-RR6uYLuBtMj4RI',
+    authDomain: 'ingreso-egreso-app-6e1b2.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-6e1b2.firebaseio.com',
+    projectId: 'ingreso-egreso-app-6e1b2',
+    storageBucket: 'ingreso-egreso-app-6e1b2.appspot.com',
+    messagingSenderId: '739814598360'
+  }
 };
 
 /*
